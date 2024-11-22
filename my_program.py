22de9571104d48ea25a0ce11938ce0f10d7fb74e
B269905-2024 <s2713107@ed.ac.uk>
2024-11-22 03:23:45 +0000

    something finally worked

--- my_program.py
+++ my_program.py
@@ -23,4 +23,21 @@ print(f'You chose {prot_fam} in {tax_gr}')
 my_query = f"{prot_fam}[protein] AND {tax_gr}[Organism]"
 print(my_query)
 
+#fetch sequences
+subprocess.call("chmod 700 fetch_sequence.sh", shell=True)
 subprocess.run(['./fetch_sequence.sh', my_query])
+
+
+print("Would you like to run analysis on this number of sequences? y/n")
+
+#align sequences and then determine, and plot, the level of conservation between the protein sequences
+print("specify alignment window size") #maybe add option of multiple sizes?
+#win = input()
+win = 3
+subprocess.call("clustalo -i my_sequence.fasta -o my_aligned_seq.fasta", shell=True)    
+subprocess.call("plotcon -sequence my_aligned_seq.fasta -winsize 3 -graph png", shell=True)
+
+
+
+
+
